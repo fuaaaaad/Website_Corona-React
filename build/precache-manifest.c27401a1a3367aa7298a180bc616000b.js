0405573d63ef9d155c055bdb612d696a5a47918c
Muhammed Fuaad <57677134+fuaaaaad@users.noreply.github.com>
2020-09-14 20:50:46 +0400

    Add files via upload

---
+++ build/precache-manifest.c27401a1a3367aa7298a180bc616000b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b3dd8db472b1c1801260a5ddbd135a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a581e4947c7557f2474",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6a9407a24a1e73b7f336",
+    "url": "/static/css/main.faa4b056.chunk.css"
+  },
+  {
+    "revision": "8a581e4947c7557f2474",
+    "url": "/static/js/2.a7ee24b2.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.a7ee24b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9407a24a1e73b7f336",
+    "url": "/static/js/main.98d324f4.chunk.js"
+  },
+  {
+    "revision": "fa267d38a49e922309e9",
+    "url": "/static/js/runtime-main.1d94c98b.js"
+  },
+  {
+    "revision": "43e395f3a38cca4a84ce0fff17d4d79a",
+    "url": "/static/media/Helpline.43e395f3.jpg"
+  },
+  {
+    "revision": "e93ed6ecfd617320a34f421cec554d9c",
+    "url": "/static/media/Hospital.e93ed6ec.jpg"
+  },
+  {
+    "revision": "b3daae2a48713fb095d114cfad7e8d0b",
+    "url": "/static/media/Med.b3daae2a.jpg"
+  },
+  {
+    "revision": "76bec615e12f000effc1b010b6db3a86",
+    "url": "/static/media/notification.76bec615.png"
+  }
+]);
